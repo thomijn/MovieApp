dc5f98112f7b217a75f8e49b53fc5c423eb2d8ae
Thomas <thomas.gertenbach99@gmail.com>
2019-12-18 23:58:59 +0100

    fixed usespring with model

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { useRef, useState, useEffect } from 'react';
-import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { Canvas, useRender, useThree, extend } from 'react-three-fiber'
-import { useSpring, a, config } from 'react-spring/three'
+import { useSpring, a } from "react-spring/three"
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import './App.css';
 
 extend({ OrbitControls })
 
 const Popcorn = () => {
+  const [hover, set] = useState(false)
+  const props = useSpring({
+    scale: hover ? [1.5, 1.5, 1.5] : [1, 1, 1],
+  })
   const [model, setModel] = useState()
   useEffect(() => {
     new GLTFLoader().load('/scene.gltf', setModel)
   })
-  // console.log(model)
+
   return (
-    model ? <primitive object={model.scene} position={[10, 5, 0]} /> : null
+    model ? <a.primitive onPointerOver={() => set(true)} onPointerOut={() => set(false)} object={model.scene} scale={props.scale} position={[1, 1, 1]} /> : null
   )
 }
 
@@ -42,14 +45,17 @@ const App = () => {
   return (
     <div className="App">
       <Canvas
-        camera={{ position: [0, 0, 10] }}
+        camera={{
+          position: [0, 0, 8],
+          fov: 75
+        }}
       >
-        <Controls />
+        {/* <Controls /> */}
         <ambientLight intensity={0.5} />
         <spotLight position={[110, 600, 150]} penumbra={1} castShadow />
         <Popcorn />
       </Canvas>
-    </div>
+    </div >
   );
 }
 
